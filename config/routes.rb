8040d31c5c6a683a5823ca011d1f48093dfa7d86
永井翔瑛 <nunukiyo@gmail.com>
2023-02-23 13:58:57 +0900

    controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :users
   resources :recipes
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+  
 end
